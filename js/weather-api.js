274c72fa94e599417b9e5826195bf795535ae2bf
Hasib787 <mr.hasib45@gmail.com>
2021-02-11 16:09:00 +0600

    added some changes

--- js/weather-api.js
+++ js/weather-api.js
@@ -1,3 +1,5 @@
-fetch("http://api.weatherstack.com/current? access_key = 2c4c223ee33d74aa7fa05dc47c56ad0c&query="+city+"&units="+units)
+const url = 'http://api.weatherstack.com/current?';
+const key ='2c4c223ee33d74aa7fa05dc47c56ad0c';
+fetch(url ,key)
 .then(res=> res.json())
 .then(data=> console.log(data))
